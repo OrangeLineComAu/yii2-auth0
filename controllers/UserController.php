1bbbf37b4d18487f2ae28d1d46836443c29ee2b6
Ian Warrington <iwarrington@orangeline.com.au>
2017-02-28 21:48:32 +1100

    Ensure Yii session configuration occurs before any session access via “native” PHP methods within auth0

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -24,24 +24,24 @@ class UserController extends \yii\web\Controller
      */
     public function actionLogin()
     {
-      if (!session_id()) {
-          Yii::trace("Starting session",  __METHOD__);
-          session_set_cookie_params(60 * 60 * 24 * 7); //seven days
-          session_start();
-      }
+        // Open session before accessing auth0
+        // to ensure Yii session is initialised
+        // which configures the native PHP settings
+        // (session_name(), $_SESSION object)
+        Yii::$app->session->open();
+
         $this->layout = $this->module->layout;
         Yii::trace("in actionLogin",  __METHOD__);
         $model = new LoginForm;
 
         $auth0 = $this->module->auth0;
-        //Yii::trace($auth0,  __METHOD__);
-        //Yii::trace($auth0->getUser(),  __METHOD__);
-        //Yii::trace($auth0->validate());
+
+        $return = Yii::$app->user->getReturnUrl();
 
         if ($auth0->getUser() && $auth0->validate()) {
             $model->login();
             Yii::trace($_SESSION,  __METHOD__);
-            return  $this->goBack();// $this->goBack();
+            return  $this->goBack($return);// $this->goBack();
         }
 
         return $this->render('login', [
@@ -55,6 +55,12 @@ class UserController extends \yii\web\Controller
      */
     public function actionLogout()
     {
+        // Open session before accessing auth0
+        // to ensure Yii session is initialised
+        // which configures the native PHP settings
+        // (session_name(), $_SESSION object)
+        Yii::$app->session->open();
+
         $this->module->auth0->logout();
         Yii::$app->user->logout();
         //session_destroy();
